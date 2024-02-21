04e2a90f23d1e447aa13b51066b3020efac4410b
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-02-21 16:55:29 -0300

    feat: syntax highlighting with prism

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
 import markdownStyles from "./markdown-styles.module.css";
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-go";
 
 type Props = {
   content: string;
 };
 
 export function PostBody({ content }: Props) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [content]);
   return (
     <div className="max-w-2xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
+        suppressHydrationWarning
       />
     </div>
   );
