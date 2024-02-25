96f42635a69096063c184710021b8359e769da7b
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-02-24 22:30:41 -0300

    hotfix: break all on cv cards

--- src/app/_components/info-card.tsx
+++ src/app/_components/info-card.tsx
@@ -7,24 +7,19 @@ interface Props {
   icon: IconType;
 }
 
+
 const InfoCard: React.FC<Props> = ({ title, content, icon }) => {
   return (
     <div
-      className="block border 
-    border-light-background dark:border-background 
-    p-4 shadow-sm 
-    dark:hover:border-accent hover:border-light-text
-    hover:ring-1 
-    dark:hover:ring-accent hover:ring-light-text 
-    focus:outline-none focus:ring"
+      className="block border border-light-background dark:border-background p-4 shadow-sm dark:hover:border-accent hover:border-light-text hover:ring-1 dark:hover:ring-accent hover:ring-light-text focus:outline-none focus:ring"
     >
-      <span className="inline-block bg-light-background dark:bg-background  p-3">
+      <span className="inline-block bg-light-background dark:bg-background p-3">
         {React.createElement(icon, {
           className: "text-light-text dark:text-text h-6 w-6",
         })}
       </span>
 
-      <h2 className="mt-2 font-bold">{title}</h2>
+      <h2 className="mt-2 font-bold max-w-full sm:max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl break-all">{title}</h2>
 
       <p className="hidden sm:mt-1 sm:block sm:text-sm">{content}</p>
     </div>
