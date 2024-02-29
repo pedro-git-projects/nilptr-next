7e69d9e704ef21ce1542d526dd8ce8f5ca9a4b28
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-02-29 13:27:00 -0300

    feat: storing pagination state on URL #5

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,25 +1,35 @@
 "use client";
 
-import { getAllPosts } from "@/lib/api";
+import { getPaginatedPosts } from "@/lib/api";
 import PostCard from "../_components/post-card";
 import { Post } from "@/interfaces/post";
 import { useEffect, useState } from "react";
-import Pagination from "../_components/pagination";
 import PlaceholderPostCard from "../_components/placeholder-post-card";
+import { POSTS_PER_PAGE } from "@/lib/constants";
+import URLPagination from "../_components/url-pagination";
 
-export default function Blog() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 4;
-  const [posts, setPosts] = useState<Array<Post> | null>(null);
+interface SearchParams {
+  page?: string;
+}
+
+interface Props {
+  searchParams?: SearchParams;
+}
+
+const Blog: React.FC<Props> = ({ searchParams }) => {
+  const currentPage = Number(searchParams?.page) || 1;
+  const [posts, setPosts] = useState<Array<Post>>([]);
   const [loading, setLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(0);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         setLoading(true);
-        const fetchedPosts = await getAllPosts();
-        setPosts(fetchedPosts);
+        const { posts, totalPages } = await getPaginatedPosts(currentPage);
+        setTotalPages(totalPages);
+        setPosts(posts);
       } catch (error) {
         // @ts-ignore
         setError(error);
@@ -31,24 +41,12 @@ export default function Blog() {
     fetchPosts();
   }, [currentPage]);
 
-  const totalPages = posts ? Math.ceil(posts.length / postsPerPage) : 0;
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts
-    ? posts.slice(indexOfFirstPost, indexOfLastPost)
-    : [];
-
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
-
   return (
     <main>
       <h1 className="text-3xl font-bold sm:text-4xl m-6">Publicações</h1>
       {loading ? (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-          {Array.from({ length: postsPerPage }).map((_, index) => (
+          {Array.from({ length: POSTS_PER_PAGE }).map((_, index) => (
             <PlaceholderPostCard key={index} />
           ))}
         </div>
@@ -56,20 +54,20 @@ export default function Blog() {
         <p>Error: {error.message}</p>
       ) : (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
-          {currentPosts.map(post => (
+          {posts.map(post => (
             <PostCard key={post.slug} post={post} />
           ))}
         </div>
       )}
       <div className="m-6">
-        {posts && totalPages > 1 && (
-          <Pagination
-            totalPages={totalPages}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-          />
+        {totalPages > 1 && (
+          <div className="m-6">
+            <URLPagination totalPages={totalPages} />
+          </div>
         )}
       </div>
     </main>
   );
-}
+};
+
+export default Blog;
